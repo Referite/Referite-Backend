b196dede8d69122c8543d71c31fe599aa5d1a3e9
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-10-18 09:35:29 +0700

    fix pep8

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from db import Sport, SportSchedule, SportType
 from Enum.sportStatus import SportStatus
 app = FastAPI()
 
+
 @app.on_event('startup')
 async def connect_db():
     client = AsyncIOMotorClient(config("MONGO_URL", cast=str, default="mongodb://localhost:27017"),
                                 tls=True,
-                                tlsAllowInvalidCertificates=True)
+                                tlsAllowIynvalidCertificates=True)
 
     await init_beanie(database=client.referee, document_models=[SportType, SportSchedule, Sport])
 
@@ -21,24 +22,25 @@ async def add_some_data():
 
     body = {
         "datetime": "2021-08-01T00:00:00",
-        "sport":[{
-        "sport_id": 1,
-        "sport_name": "Football",
-        "sport_type": [
-            {
-                "type_id": 1,
-                "type_name": "11v11",
-                "status": SportStatus.CEREMONIES
-            },
-            {
-                "type_id": 2,
-                "type_name": "7v7",
-                "status": SportStatus.TROPHY
-            }
-        ]}]
+        "sport": [{
+            "sport_id": 1,
+            "sport_name": "Football",
+            "sport_type": [
+                {
+                    "type_id": 1,
+                    "type_name": "11v11",
+                    "status": SportStatus.CEREMONIES
+                },
+                {
+                    "type_id": 2,
+                    "type_name": "7v7",
+                    "status": SportStatus.TROPHY
+                }
+            ]}]
     }
 
     await SportSchedule(**body).insert()
+
 
 @app.get('/2')
 async def add_data():
