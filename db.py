843c4c9d3d6e72c69cdb0a86f910a4f469dc970d
Siravich Termvadsayanon <93331101+koonwill@users.noreply.github.com>
2023-10-27 12:16:06 +0700

    solve conversation

--- db.py
+++ db.py
@@ -7,5 +7,4 @@ mongo_connection = client['referee']
 
 sport_schedule_connection = mongo_connection['SportSchedule']
 sport_connection = mongo_connection['Sport']
-sport_type_connection = mongo_connection['SportType']
 referee_id_connection = mongo_connection['RefereeID']
