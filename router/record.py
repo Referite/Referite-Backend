170b9cb8859f3bb88305edd11bb2f23db2fa3367
PanupunJanin <panupan.ja@gmail.com>
2023-11-06 00:19:48 +0700

    Fix the logic and make it return message like mock up.

--- router/record.py
+++ router/record.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models import RecordBody, VerifyBody
 from utils import error_handler
 from db import sport_schedule_connection
-from controllers.record_controller import get_ioc_data, find_date_of_that_sport_type, record_medal_default_restriction, record_medal_repechage_restriction
-
+from controllers.record_controller import get_ioc_data, find_date_of_that_sport_type, record_medal_default_restriction, \
+    record_medal_repechage_restriction
 
 router = APIRouter(
     prefix='/api/record',
@@ -17,10 +17,13 @@ router = APIRouter(
 def get_detail(sport_id: int):
     """Retrieve sport detail by sport ID and match it with schedule data."""
     resp = get_ioc_data(sport_id)
-    current_schedule = list(sport_schedule_connection.find(filter={"sport.sport_id": sport_id, "sport.sport_type.type_id": 2}, projection={'_id': 0, 'sport.sport_type._id': 0, 'sport.revision_id': 0, 'sport.sport_type.revision_id': 0, 'sport._id': 0}))
+    current_schedule = list(
+        sport_schedule_connection.find(filter={"sport.sport_id": sport_id, "sport.sport_type.type_id": 2},
+                                       projection={'_id': 0, 'sport.sport_type._id': 0, 'sport.revision_id': 0,
+                                                   'sport.sport_type.revision_id': 0, 'sport._id': 0}))
 
     for types in resp['sport_types']:
-         types["competition_date"] = find_date_of_that_sport_type(current_schedule, types['type_id'])
+        types["competition_date"] = find_date_of_that_sport_type(current_schedule, types['type_id'])
 
     return resp
 
@@ -30,27 +33,24 @@ def get_detail(sport_id: int):
 def verify_medal(verify_body: VerifyBody):
     """
     Record medal verification if it meets any restrictions and warn accordingly
-    return twos dict: warning, message in this order
-    warning = {"Warning": "message"} or warning = {} depends on if it needs to warn or not
-    message = {"Message": "message"}
+    return a message dict: warning, message in this order or HTTP400 if invalid
+    message = {"Message": "message", "Monosport": "Appear if have only 1 participant"}
     """
     verify = verify_body.model_dump()
-    sport_name, participant = verify['sport_name'], verify['participant']
+    sport_name, participants = verify['sport_name'], verify['participants']
+    if not participants:
+        raise HTTPException(400, "The participants can not be empty")
     repechage_list = ["wrestling", "boxing", "judo", "taekwondo"]
-    warning_countries = []
-    message = {"Warning": "", "Message": "Medal allocation successful."}
-    for country in participant:
-        country_name = country['country']
-        gold = country['medal']['gold']
-        silver = country['medal']['silver']
-        bronze = country['medal']['bronze']
-        if sport_name.lower() in repechage_list:
-            warning_country = record_medal_repechage_restriction(country_name, gold, silver, bronze)
-            warning_countries.append(warning_country)
-        else:
-            warning_country = record_medal_default_restriction(country_name, gold, silver, bronze)
-            if warning_country != '':
-                warning_countries.append(warning_country)
-    if warning_countries:
-        message["Warning"] = f"Medal allocation for {warning_countries} deviates from default logic."
+    gold, silver, bronze = 0, 0, 0
+    for country in participants:
+        gold += country['medal']['gold']
+        silver += country['medal']['silver']
+        bronze += country['medal']['bronze']
+    if sport_name.lower() in repechage_list:
+        message = record_medal_repechage_restriction(gold, silver, bronze)
+    else:
+        message = record_medal_default_restriction(gold, silver, bronze)
+    if len(participants) == 1:
+        message["Monosport"] = (f"There are only {len(participants)} country in this medal allocation, "
+                                f"Do you want to confirm this record?")
     return message
