476cd21e95683c2f33571b846f68942fcb013141
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-06 21:04:35 +0700

    hotfix

--- utils.py
+++ utils.py
@@ -20,12 +20,15 @@ def calculate_sport_status(sport_types: List[Dict]):
         TROPHY -> any sport_type.status is trophy it will be trophy,
         RECORDED -> all sport_type.status must be recorded then it will be
     """
+
+    if not sport_types:
+        return None
+
     for sport_type in sport_types:
         if sport_type["status"] == f"{SportStatus.CEREMONIES}":
             return SportStatus.CEREMONIES
-        elif sport_type["status"] == f"{SportStatus.COMPETITIVE}":
+        if sport_type["status"] == f"{SportStatus.COMPETITIVE}":
             return SportStatus.COMPETITIVE
-        elif sport_type["status"] == f"{SportStatus.TROPHY}":
+        if sport_type["status"] == f"{SportStatus.TROPHY}":
             return SportStatus.TROPHY
-        else:
-            return None # Change to empty or something
+    return SportStatus.RECORDED
