9627fe09ad8024e080e2da14c690551b33915594
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-11-08 17:26:09 +0700

    fix find date and status

--- controllers/record_controller.py
+++ controllers/record_controller.py
@@ -49,14 +49,17 @@ def find_date_of_that_sport_type(schedule_data, type_id, sport_id):
     raise HTTPException(400)
 
 
-def find_status_of_that_sport_type(schedule_data, type_id):
+def find_status_of_that_sport_type(schedule_data, type_id, sport_id):
     """
     Find status of that sport type in schedule data
     """
     for schedule in schedule_data:
         for sport in schedule["sport"]:
+            if sport["sport_id"] != sport_id:
+                continue
             for sport_type in sport["sport_type"]:
                 if sport_type["type_id"] == type_id:
+                    print(sport_type)
                     return sport_type["status"]
     raise Exception("No sport that matches you request type_id")
 
