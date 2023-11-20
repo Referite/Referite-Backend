a401f913138d44c7bb8b48c637a4225d0a37505c
koonwill <93331101+koonwill@users.noreply.github.com>
2023-11-20 17:08:46 +0700

    Extract Method and externalize

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,62 +1,58 @@
 import requests
 import unittest
 from db import referee_id_connection
+from decouple import config
+
+TEST_URL = config("TEST_URL", default="https://referite-6538ffaf77b0.herokuapp.com/")
+AUDIENCE_TOKEN = config("AUDIENCE_TOKEN")
 
 class TestLogin(unittest.TestCase):
     def setUp(self):
-        self.url = 'https://referite-6538ffaf77b0.herokuapp.com/'
-        # self.url = 'http://127.0.0.1:8000/'
-        self.audienceToken = '02e2cdc6ac5d17a2bb67824c91f51ac55ce46465133f92233e3daa552120bcb3'
+        self.url = TEST_URL
+        self.audienceToken = AUDIENCE_TOKEN
 
-    def test_login_valid_username_password(self):
-        """Test login with valid username and password"""
-        data = {'username': 'referee', 'password': 'referee123'}
-        r = requests.post(
-            f'{self.url}api/auth/token',
+    def get_handler(self, url, token):
+        return requests.get(
+            f'{self.url}{url}',
+            headers={
+                'authorization': token
+            }
+        )
+
+    def post_handler(self, url, data):
+        return requests.post(
+            f'{self.url}{url}',
             data=data,
             headers={
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         )
+
+    def test_login_valid_username_password(self):
+        """Test login with valid username and password"""
+        data = {'username': 'referee', 'password': 'referee123'}
+        r = self.post_handler('api/auth/token', data)
         self.assertEqual(r.status_code, 201)
         self.assertEqual(r.json()['expired'], referee_id_connection.find_one({'username': data['username']})['expired'])
 
     def test_login_valid_username_invalid_password(self):
         """Test login with valid username and invalid password"""
         data = {'username': 'referee', 'password': 'referee'}
-        r = requests.post(
-            f'{self.url}api/auth/token',
-            data=data,
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
+        r = self.post_handler('api/auth/token', data)
         self.assertEqual(r.status_code, 401)
         self.assertEqual(r.text, '{"detail":"Please, Login"}')
 
     def test_login_invalid_username_valid_password(self):
         """Test login with invalid username and valid password"""
         data = {'username': 'tester', 'password': 'referee123'}
-        r = requests.post(
-            f'{self.url}api/auth/token',
-            data=data,
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
+        r = self.post_handler('api/auth/token', data)
         self.assertEqual(r.status_code, 401)
         self.assertEqual(r.text, '{"detail":"Please, Login"}')
 
     def test_login_invalid_username_invalid_password(self):
         """Test login with invalid username and invalid password"""
         data = {'username': 'tester', 'password': 'referee'}
-        r = requests.post(
-            f'{self.url}api/auth/token',
-            data=data,
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
+        r = self.post_handler('api/auth/token', data)
         self.assertEqual(r.status_code, 401)
         self.assertEqual(r.text, '{"detail":"Please, Login"}')
 
@@ -64,77 +60,38 @@ class TestLogin(unittest.TestCase):
     def test_login_empty_username_empty_password(self):
         """Test login with empty username and empty password"""
         data = {'username': '', 'password': ''}
-        r = requests.post(
-            f'{self.url}api/auth/token',
-            data=data,
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
+        r = self.post_handler('api/auth/token', data)
         self.assertEqual(r.status_code, 401)
         self.assertEqual(r.text, '{"detail":"Please, Login"}')
 
     def test_login_special_symbol(self):
         """Test login with special symbol"""
         data = {'username': 'referee', 'password': 'referee!@#$%^&*()'}
-        r = requests.post(
-            f'{self.url}api/auth/token',
-            data=data,
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
+        r = self.post_handler('api/auth/token', data)
         self.assertEqual(r.status_code, 401)
         self.assertEqual(r.text, '{"detail":"Please, Login"}')
 
     def test_login_and_check_permission(self):
         """Test login with valid username and password then check permission"""
         data = {'username': 'referee', 'password': 'referee123'}
-        r = requests.post(
-            f'{self.url}api/auth/token',
-            data=data,
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
-        token = r.json()['access_token']
-        r = requests.get(
-            f'{self.url}api/schedule/all',
-            headers={
-                'authorization': token
-            }
-        )
-        self.assertEqual(r.status_code, 200)
+        r = self.post_handler('api/auth/token', data)
+        r1 = self.get_handler('api/schedule/all', r.json()['access_token'])
+        self.assertEqual(r1.status_code, 200)
 
     def test_permission(self):
         """Test permission for wrong authorization and audience authorization"""
         lst_url = ["api/schedule/all", "api/schedule/sport"]
         for url in lst_url:
-            r = requests.get(
-                f'{self.url}{url}',
-                headers={
-                    'authorization': 'Toast'
-                }
-            )
+            r = self.get_handler(url, 'Toast')
             self.assertEqual(r.status_code, 401)
             self.assertEqual(r.text, '{"detail":"Please, Login"}')
-            r1 = requests.get(
-                f'{self.url}{url}',
-                headers={
-                    'authorization': self.audienceToken
-                }
-            )
+            r1 = self.get_handler(url, self.audienceToken)
             self.assertEqual(r1.status_code, 200)
 
     def test_audience_permission(self):
         """Test permission for other page that audience cannot access"""
         # GET
-        r = requests.get(
-            f'{self.url}{"api/record/detail/2024-08-01T00:00:00/1"}',
-            headers={
-                'authorization': self.audienceToken
-            }
-        )
+        r = self.get_handler('api/record/detail/2024-08-01T00:00:00/1', self.audienceToken)
         self.assertEqual(r.status_code, 401)
         self.assertEqual(r.text, '{"detail":"Please, Login"}')
         # POST
@@ -146,7 +103,6 @@ class TestLogin(unittest.TestCase):
                     'authorization': self.audienceToken
                 }
             )
-            print(r.text)
             self.assertEqual(r.status_code, 401)
             self.assertEqual(r.text, '{"detail":"Please, Login"}')
 
