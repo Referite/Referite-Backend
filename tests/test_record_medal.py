8b3ffd15d581305857bdfb826bc5a6a7e9939dd1
PanupunJanin <panupan.ja@gmail.com>
2023-11-21 00:54:15 +0700

    Reformat all test cases

--- tests/test_record_medal.py
+++ tests/test_record_medal.py
@@ -1,113 +1,117 @@
-import unittest
-from router.record import verify_medal
-from models import VerifyBody
+from unittest import TestCase
+from .utils import get_handler, login_post_handler, data_post_handler
+from typing import Dict, List
+from decouple import config
 
 
-class TestRecordMedal(unittest.TestCase):
+def create_participant(country_name: str, gold: int, sliver: int, bronze: int) -> Dict:
+    return {"country": country_name, "medal": {"gold": gold, "silver": sliver, "bronze": bronze}}
+
+
+def create_verify_body(sport_name: str, participants: List) -> Dict:
+    return {"sport_name": sport_name, "participants": participants}
+
+
+class TestRecordMedal(TestCase):
     def setUp(self):
-        self.default_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "China", "medal": {"gold": 0, "silver": 1, "bronze": 0}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 0, "bronze": 1}}, ])
-        self.repechage_verify = VerifyBody(sport_name="Boxing", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "India", "medal": {"gold": 0, "silver": 1, "bronze": 0}},
-            {"country": "China", "medal": {"gold": 0, "silver": 0, "bronze": 1}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 0, "bronze": 1}}, ])
-        self.tie_gold_medal_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "China", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 0, "bronze": 1}}, ])
-        self.tie_silver_medal_verify = VerifyBody(sport_name="Boxing", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "India", "medal": {"gold": 0, "silver": 0, "bronze": 1}},
-            {"country": "China", "medal": {"gold": 0, "silver": 1, "bronze": 0}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 1, "bronze": 0}}, ])
-        self.exceeded_medal_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "India", "medal": {"gold": 0, "silver": 0, "bronze": 1}},
-            {"country": "China", "medal": {"gold": 0, "silver": 0, "bronze": 1}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 1, "bronze": 0}}, ])
-        self.exceeded_repechage_medal_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "India", "medal": {"gold": 0, "silver": 0, "bronze": 1}},
-            {"country": "China", "medal": {"gold": 0, "silver": 0, "bronze": 1}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 1, "bronze": 0}},
-            {"country": "Laos", "medal": {"gold": 0, "silver": 0, "bronze": 1}}, ])
-        self.impossible_silver_and_bronze_medal_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "China", "medal": {"gold": 1, "silver": 0, "bronze": 1}},
-            {"country": "Japan", "medal": {"gold": 1, "silver": 1, "bronze": 0}}, ])
-        self.impossible_silver_medal_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "China", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 1, "bronze": 0}}, ])
-        self.impossible_bronze_medal_verify = VerifyBody(sport_name="Boxing", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 0, "bronze": 0}},
-            {"country": "India", "medal": {"gold": 0, "silver": 1, "bronze": 1}},
-            {"country": "China", "medal": {"gold": 0, "silver": 1, "bronze": 0}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 1, "bronze": 0}}, ])
-        self.mono_sport_verify = VerifyBody(sport_name="Archery", participants=[
-            {"country": "Thailand", "medal": {"gold": 1, "silver": 1, "bronze": 1}}, ])
-        self.zero_medal_verify = VerifyBody(sport_name="Boxing", participants=[
-            {"country": "Thailand", "medal": {"gold": 0, "silver": 0, "bronze": 0}},
-            {"country": "India", "medal": {"gold": 0, "silver": 0, "bronze": 0}},
-            {"country": "China", "medal": {"gold": 0, "silver": 0, "bronze": 0}},
-            {"country": "Japan", "medal": {"gold": 0, "silver": 0, "bronze": 0}}, ])
-        self.empty_verify = VerifyBody(sport_name="Archery", participants=[])
+        self.VERIFY_PATH = 'api/record/verify'
+        self.TOKEN = config("DEV_TOKEN")
+        self.default_verify = create_verify_body("Archery", [create_participant("Thailand", 1, 0, 0),
+                                                             create_participant("China", 0, 1, 0),
+                                                             create_participant("Japan", 0, 0, 1)])
+        self.repechage_verify = create_verify_body("Boxing", [create_participant("Thailand", 1, 0, 0),
+                                                              create_participant("India", 0, 1, 0),
+                                                              create_participant("China", 0, 0, 1),
+                                                              create_participant("Japan", 0, 0, 1)])
+        self.tie_gold_medal_verify = create_verify_body("Archery", [create_participant("Thailand", 1, 0, 0),
+                                                                    create_participant("China", 1, 0, 0),
+                                                                    create_participant("Japan", 0, 0, 1)])
+        self.tie_silver_medal_verify = create_verify_body("Boxing", [create_participant("Thailand", 1, 0, 0),
+                                                                     create_participant("India", 0, 0, 1),
+                                                                     create_participant("China", 0, 1, 0),
+                                                                     create_participant("Japan", 0, 1, 0)])
+        self.exceeded_medal_verify = create_verify_body("Archery", [create_participant("Thailand", 1, 0, 0),
+                                                                    create_participant("India", 0, 0, 1),
+                                                                    create_participant("China", 0, 0, 1),
+                                                                    create_participant("Japan", 0, 1, 0)])
+        self.exceeded_repechage_medal_verify = create_verify_body("Boxing", [create_participant("Thailand", 1, 0, 0),
+                                                                             create_participant("India", 0, 0, 1),
+                                                                             create_participant("China", 0, 0, 1),
+                                                                             create_participant("Japan", 0, 1, 0),
+                                                                             create_participant("Laos", 0, 0, 1), ])
+        self.impossible_silver_and_bronze_medal_verify = create_verify_body("Archery",
+                                                                            [create_participant("Thailand", 1, 0, 0),
+                                                                             create_participant("China", 1, 1, 0),
+                                                                             create_participant("Japan", 1, 0, 1)])
+        self.impossible_silver_medal_verify = create_verify_body("Archery", [create_participant("Thailand", 1, 0, 0),
+                                                                             create_participant("China", 1, 0, 0),
+                                                                             create_participant("Japan", 0, 1, 0)])
+        self.impossible_bronze_medal_verify = create_verify_body("Boxing", [create_participant("Thailand", 1, 0, 0),
+                                                                            create_participant("India", 0, 1, 1),
+                                                                            create_participant("China", 0, 1, 0),
+                                                                            create_participant("Japan", 0, 1, 0)])
+        self.mono_sport_verify = create_verify_body("Archery", [create_participant("Thailand", 1, 0, 0), ])
+        self.zero_medal_verify = create_verify_body("Boxing", [create_participant("Thailand", 0, 0, 0),
+                                                               create_participant("India", 0, 0, 0),
+                                                               create_participant("China", 0, 0, 0),
+                                                               create_participant("Japan", 0, 0, 0)])
+        self.empty_verify = create_verify_body("Archery", [])
 
     def test_success_medal_allocation(self):
         """Test if the success medal allocation return message correctly."""
-        message = verify_medal(self.default_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful."})
-        message = verify_medal(self.repechage_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful."})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.default_verify)
+        self.assertEqual(200, request.status_code)
+        self.assertEqual('{"Message":"Medal allocation successful."}', request.text)
 
     def test_tie_medal_warning(self):
         """Test if the tie medal scenario allocation return message correctly."""
-        message = verify_medal(self.tie_gold_medal_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful.",
-                                   "Warning": f"There are 2 gold medals awarded, Do you want to confirm this record?"})
-        message = verify_medal(self.tie_silver_medal_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful.",
-                                   "Warning": f"There are 2 silver medals awarded, "
-                                              f"Do you want to confirm this record?"})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.tie_gold_medal_verify)
+        self.assertEqual(200, request.status_code)
+        self.assertEqual('{"Message":"Medal allocation successful.","Warning":"There are 2 gold medals awarded, '
+                         'Do you want to confirm this record?"}', request.text)
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.tie_silver_medal_verify)
+        self.assertEqual(200, request.status_code)
+        self.assertEqual('{"Message":"Medal allocation successful.","Warning":"There are 2 silver medals awarded, '
+                         'Do you want to confirm this record?"}', request.text)
 
     def test_exceeded_default_logic_medal_allocation(self):
         """Test if the returned warning message is correct if the total amount is exceeded."""
-        message = verify_medal(self.exceeded_medal_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful.",
-                                   "Warning": f"There are 4 medals awarded, Do you want to confirm this record?"})
-        message = verify_medal(self.exceeded_repechage_medal_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful.",
-                                   "Warning": f"There are 5 medals awarded, Do you want to confirm this record?"})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.exceeded_medal_verify)
+        self.assertEqual(200, request.status_code)
+        self.assertEqual( '{"Message":"Medal allocation successful.","Warning":"There are 4 medals awarded, '
+                          'Do you want to confirm this record?"}', request.text)
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.exceeded_repechage_medal_verify)
+        self.assertEqual(200, request.status_code)
+        self.assertEqual('{"Message":"Medal allocation successful.","Warning":"There are 5 medals awarded, '
+                         'Do you want to confirm this record?"}', request.text)
 
     def test_mono_sport_medal_allocation_warning(self):
         """Test if the mono sport message return when only one country participated in the event."""
-        message = verify_medal(self.mono_sport_verify)
-        self.assertEqual(message, {"Message": "Medal allocation successful.",
-                                   "Monosport": 'There are only 1 country in this medal allocation, '
-                                                'Do you want to confirm this record?'})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.mono_sport_verify)
+        self.assertEqual(200, request.status_code)
+        self.assertEqual('{"Message":"Medal allocation successful.","Monosport":"There are only 1 country in this '
+                         'medal allocation, Do you want to confirm this record?"}', request.text)
 
     def test_400_bad_request_return_when_the_inputted_body_is_invalid(self):
         """Test if HTTP400 returned when inputting invalid body"""
-        message = verify_medal(self.impossible_silver_and_bronze_medal_verify)
-        self.assertEqual(message, {'status': 'error', 'message': "(400, 'There are 3 gold medals awarded, "
-                                                                 "No silver or bronze medal will be given.')"})
-        message = verify_medal(self.impossible_silver_medal_verify)
-        self.assertEqual(message, {'status': 'error', 'message': "(400, 'There are 2 gold medals awarded, "
-                                                                 "No silver medal will be given.')"})
-        message = verify_medal(self.impossible_bronze_medal_verify)
-        self.assertEqual(message, {'status': 'error', 'message': "(400, 'There are 3 silver medals awarded, "
-                                                                 "No bronze medal will be given.')"})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.impossible_silver_and_bronze_medal_verify)
+        self.assertEqual(400, request.status_code)
+        self.assertEqual('{"detail":"There are 3 gold medals awarded, No silver or bronze medal will be given."}', request.text)
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.impossible_silver_medal_verify)
+        self.assertEqual(400, request.status_code)
+        self.assertEqual('{"detail":"There are 2 gold medals awarded, No silver medal will be given."}', request.text)
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.impossible_bronze_medal_verify)
+        self.assertEqual(400, request.status_code)
+        self.assertEqual('{"detail":"There are 3 silver medals awarded, No bronze medal will be given."}', request.text)
 
     def test_400_bad_request_return_when_the_total_medal_given_is_zero(self):
         """Test if HTTP400 returned when the inputted total medals is 0"""
-        message = verify_medal(self.zero_medal_verify)
-        self.assertEqual(message, {'status': 'error', 'message': "(400, 'There are 0 medals awarded, "
-                                                                 "no need to record this body.')"})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.zero_medal_verify)
+        self.assertEqual(400, request.status_code)
+        self.assertEqual('{"detail":"There are 0 medals awarded, no need to record this body."}', request.text)
 
     def test_400_bad_request_return_when_the_inputted_body_is_empty(self):
         """Test if HTTP400 returned when inputting empty body"""
-        message = verify_medal(self.empty_verify)
-        self.assertEqual(message, {'status': 'error', 'message': "(400, 'The participants can not be empty')"})
+        request = data_post_handler(self.VERIFY_PATH, self.TOKEN, data=self.empty_verify)
+        self.assertEqual(400, request.status_code)
+        self.assertEqual('{"detail":"The participants can not be empty"}', request.text)
