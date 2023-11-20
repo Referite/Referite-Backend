6434ca28259859e6d8f2419b2fd5a8df164ce263
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-21 00:57:21 +0700

    fix flake8

--- router/auth.py
+++ router/auth.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta
 import sys
 from pathlib import Path
-import time
 
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from db import referee_id_connection
 
@@ -12,7 +11,7 @@ from auth.auth_handler import (
     check_user,
     create_access_token,
     check_token,
-    logout_handler
+    logout_handler,
 )
 from models import TokenBody
 from utils import error_handler
@@ -28,6 +27,7 @@ router = APIRouter(
 expire_time = timedelta(days=1)
 date = (datetime.now() + expire_time).isoformat()
 
+
 @error_handler
 @router.post("/token", status_code=201, response_model=TokenBody)
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -37,15 +37,19 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     """
     if check_user(form_data) and check_password(form_data):
         access_token = create_access_token(form_data.username)
-        referee_id_connection.update_one({"username": form_data.username}, {"$set": {"expired": date}})
+        referee_id_connection.update_one(
+            {"username": form_data.username}, {"$set": {"expired": date}}
+        )
         return {"access_token": access_token, "expired": date}
     raise HTTPException(401, "Please, Login")
+
 
 @error_handler
 @router.get("/logout", dependencies=[Depends(logout_handler)])
 def logout():
     """Delete token"""
     return {"message": "Logout Successfully"}
+
 
 @router.get("/test", dependencies=[Depends(check_token)])
 def toast():
